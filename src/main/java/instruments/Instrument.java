9181a47ca63322c716f5d3f1524eee0447619faf
Marcin <marcinszydelko@yahoo.com>
2020-01-20 00:28:23 +0000

    create Instrument abstract class

---
+++ src/main/java/instruments/Instrument.java
@@ -0,0 +1,19 @@
+package instruments;
+
+public abstract class Instrument {
+    private String material;
+    private String colour;
+    private InstrumentType instrumentType;
+    private int sellPrice;
+    private int buyPrice;
+
+    public Instrument(String material, String colour, InstrumentType instrumentType, int sellPrice, int buyPrice) {
+        this.material = material;
+        this.colour = colour;
+        this.instrumentType = instrumentType;
+        this.sellPrice = sellPrice;
+        this.buyPrice = buyPrice;
+    }
+
+    
+}
