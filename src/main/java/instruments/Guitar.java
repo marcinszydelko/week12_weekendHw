d5a87074e20d06096fc328c47d015165e05a479a
Marcin <marcinszydelko@yahoo.com>
2020-01-20 00:33:34 +0000

    create Guitar and Trumpet class

---
+++ src/main/java/instruments/Guitar.java
@@ -0,0 +1,10 @@
+package instruments;
+
+public class Guitar extends Instrument {
+    private int numberOfStrings;
+
+    public Guitar(String material, String colour, InstrumentType instrumentType, int sellPrice, int buyPrice, int numberOfStrings) {
+        super(material, colour, instrumentType, sellPrice, buyPrice);
+        this.numberOfStrings = numberOfStrings;
+    }
+}
---
+++ src/main/java/instruments/Trumpet.java
@@ -0,0 +1,10 @@
+package instruments;
+
+public class Trumpet extends Instrument{
+    private int numberOfValves;
+
+    public Trumpet(String material, String colour, InstrumentType instrumentType, int sellPrice, int buyPrice, int numberOfValves) {
+        super(material, colour, instrumentType, sellPrice, buyPrice);
+        this.numberOfValves = numberOfValves;
+    }
+}
